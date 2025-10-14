b5abedce67d0c80cccdea39b88b3d24b139d47f2
Rodney Mbasu <rodneymbasu@gmail.com>
2025-10-14 19:52:10 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api/`, 
+  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
 });
 
 export default api;
+
